de93cdc678f6fea9e082cb8d7c30444ccd0d7796
emirali-cell <emirali@student.net.nz>
2026-06-10 12:14:01 +1200

    Crocodile
    Coded movement upwards.
    Coded opening of jaws.

--- Assets/Scripts/CrocodileScript.cs
+++ Assets/Scripts/CrocodileScript.cs
@@ -1,10 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class CrocodileScript : MonoBehaviour
 {
     public int rotatorValue = -1;
+    private float timer = 0f;
+    public Rigidbody2D rigidBody;
+    private UnityEngine.Vector2 movement;
+    public float targetAngle = 90f;
+    float r;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +21,19 @@ public class CrocodileScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer +=Time.deltaTime;
+
+        movement.y = timer * 2;
+
+        if (timer >= 4)
+        {
+            movement.y = 0 * rigidBody.velocity.y;
+
+            float Angle = Mathf.SmoothDampAngle(transform.eulerAngles.z, targetAngle, ref r, 0.5f);
+
+            transform.rotation = Quaternion.Euler(0,0, Angle);
+        }
+
+        rigidBody.velocity = movement;
     }
 }
